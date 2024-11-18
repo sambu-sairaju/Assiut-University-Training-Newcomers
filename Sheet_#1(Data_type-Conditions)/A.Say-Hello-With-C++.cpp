fcbcc3ffb3bc2858f839b35ff2e4456fafeb905d
sambu-sairaju <sambusairaju@gmail.com>
2024-11-18 21:25:04 +0530

    first_commit

---
+++ Sheet_#1(Data_type-Conditions)/A.Say-Hello-With-C++.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cout << "Hello, " << s << endl;
+    return 0;
+}
---
+++ Sheet_#1(Data_type-Conditions)/B.Basic-Data-Types.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << d << endl
+         << e << endl;
+    return 0;
+}
