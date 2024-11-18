221e23c4ada40fc95f0b0dbda5ddd89f5a26d079
sambu-sairaju <sambusairaju@gmail.com>
2024-11-18 22:26:13 +0530

    initail

---
+++ E.Area of a Circle.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double pi = 3.141592653;
+    double r;
+    cin >> r;
+    long double result = pi * r * r;
+    cout << fixed << setprecision(9) << result << endl;
+}
