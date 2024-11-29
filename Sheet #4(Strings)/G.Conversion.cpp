c50c05fd2dab690f1ef4a0d3488e84a55b4ab6d9
sambu-sairaju <sambusairaju@gmail.com>
2024-11-29 22:33:38 +0530

    initial

---
+++ Sheet #4(Strings)/G.Conversion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ',')
+            s[i] = ' ';
+        else if (s[i] >= 65 && s[i] <= 90)
+            s[i] += 32;
+        else
+            s[i] -= 32;
+    }
+    cout << s << endl;
+    return 0;
+}
---
+++ Sheet #4(Strings)/H.Good-or-Bad.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int flag = 0;
+        for (int i = 0; i <= s.length() - 3; i++)
+        {
+            if (s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '0')
+            {
+                cout << "Good" << endl;
+                flag = 1;
+                break;
+            }
+            else if (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '1')
+            {
+                cout << "Good" << endl;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            cout << "Bad" << endl;
+    }
+    return 0;
+}
