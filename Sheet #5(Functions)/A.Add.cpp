c36bedbf0618cb299b72e097e0ceb0f6291aa25f
sambu-sairaju <sambusairaju@gmail.com>
2024-12-13 10:39:50 +0530

    initial

--- Sheet #5(Functions)/A.Add.cpp
+++ Sheet #5(Functions)/A.Add.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+void Add(int x, int y)
+{
+    cout << x + y << "\n";
+    return;
+}
 int main()
 {
     ios::sync_with_stdio(false);
@@ -8,6 +13,8 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    
+    int x, y;
+    cin >> x >> y;
+    Add(x, y);
     return 0;
 }
--- Sheet #5(Functions)/B.Print.cpp
+++ Sheet #5(Functions)/B.Print.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+void print(int n)
+{
+    for (int i = 1; i < n; i++)
+        cout << i << " ";
+    cout << n << endl;
+    return;
+}
 int main()
 {
     ios::sync_with_stdio(false);
@@ -8,6 +15,8 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    
+    int n;
+    cin >> n;
+    print(n);
     return 0;
 }
